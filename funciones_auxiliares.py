16bd7096b8f9fd1f909617cf0575b2fba98bae77
diazvaned <83316111+diazvaned@users.noreply.github.com>
2021-06-08 20:33:56 -0300

    Update funciones_auxiliares.py

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -43,5 +43,20 @@ def modulo(numero):
         numero = -numero
     return numero
         
+def es_palindromo(texto):
+    inicio = 0
+    fin = len(texto) -1
+    contador = 0
+    control = 0
+    while ((texto[inicio] != texto[fin]) and (control != len(texto)-1)):
+        if (texto[inicio] == texto[fin]):
+            contador = contador +1
+        inicio = inicio +1
+        fin = fin -1
+        control = control +1
+    if (contador == control):
+        return True
+    else:
+        return False
         
         
