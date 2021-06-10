26051e1ddb07d25985957346544d757bd4727abe
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-10 19:28:17 -0300

    Update funciones_auxiliares.py
    added "lista_random(cantidad, numero_minimo, numero_maximo)"

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -6,6 +6,8 @@ class FueraDeLimite(Exception):
     """Esta es la Excepcion para el ingreso incorrecto por salirse de los limites"""
     pass
 
+from random import randint
+
 def ingrese_numero(mensaje):
     ingresado = input(mensaje+":")
     try:
@@ -59,4 +61,15 @@ def es_palindromo(texto):
     else:
         return False
         
-        
+
+def lista_random(cantidad=10, numero_minimo=0, numero_maximo=100):
+    """
+    Esta función genera una lista de `cantidad` 
+    con valores entre `número_minimo` y `número_maximo`
+    Con valores por defecto sensibles para su uso rápido
+        (10 elementos, entre 0 y 100)
+    """
+    miliasta = list()
+    for i in range(cantidad):
+        milista.append(randint(numero_minimo, numero_maximo))
+    return milista      
