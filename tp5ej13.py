470e6c991844496038a11456c315d2efcb077500
EstebanCapolicchio <84167593+EstebanCapolicchio@users.noreply.github.com>
2021-06-10 18:17:11 -0300

    Create tp5ej13.py
    added functions and tester

---
+++ tp5ej13.py
@@ -0,0 +1,25 @@
+class ElementoNoEncontrado(Exception):
+    """Esta es la Excepcion para cuando no se encuentra un elemento"""
+    pass
+        
+def encuentra(texto1, texto2):
+    texto2_primer_caracter = texto2[0]
+    texto1_longitud = len(texto1)
+    texto2_longitud = len(texto2)
+    for i in range(0 , texto1_longitud):
+        if(texto1[i] == texto2_primer_caracter):
+            if(texto1[i:i + texto2_longitud] == texto2):
+                return i
+    raise ElementoNoEncontrado()
+
+
+
+def prueba():
+    texto1 = input("ingrese texto: ")
+    texto2 = input("ingrese palabra a buscar: ")
+    try:
+        posicion = encuentra(texto1 , texto2)
+        print(f"[{texto2}] se encuentra en la posicion [{posicion}] del texto [{texto1}]")
+    except ElementoNoEncontrado:
+        raise ElementoNoEncontrado(f"No se ha encontrado [{b}] en [{a}]")
+ 
